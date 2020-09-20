b2d704f3baca73e686c9826b6197fbb8d5d5e366
Laercio Santos <laerciosantosn@gmail.com>
2020-09-20 14:36:22 -0300

    Criando pagina de listagem

---
+++ front-web/src/pages/Records/Pagination/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import './styles.css';
+
+type Props = {
+  // eslint-disable-next-line react/require-default-props
+  totalPages?: number;
+  // eslint-disable-next-line
+  goToPage: Function;
+  activePage: number;
+};
+
+const Pagination = ({ totalPages = 0, goToPage, activePage }: Props) => {
+  const paginationItems = Array.from(Array(totalPages).keys());
+
+  return (
+    <div className="pagination-container">
+      {paginationItems.map(item => (
+        <button
+          key={item}
+          type="button"
+          className={`pagination-item ${
+            activePage === item ? 'active' : 'inactive'
+          }`}
+          onClick={() => goToPage(item)}
+        >
+          {item + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
